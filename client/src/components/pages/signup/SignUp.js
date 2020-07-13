abda5a3a08a502ee3934000a91f5c4ed61062ea9
dasloinc <59852619+dasloinc@users.noreply.github.com>
2020-07-13 15:37:03 -0400

    changed to AuthService

--- client/src/components/pages/signup/SignUp.js
+++ client/src/components/pages/signup/SignUp.js
@@ -1,5 +1,5 @@
 import React, {useState,useRef,useEffect} from 'react';
-import AuthService from '../../../Services/AuthService';
+import AuthService from '../../../AuthServices/AuthService';
 import Message from '../../Message';
 import "../../css/sign.css";
 
