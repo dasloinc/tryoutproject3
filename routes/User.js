b418b61e5a1a2dd5ff6991375c7835b0e5bb845b
dasloinc <59852619+dasloinc@users.noreply.github.com>
2020-07-14 14:09:56 -0400

    push 4433

--- routes/User.js
+++ routes/User.js
@@ -32,6 +32,23 @@ userRouter.post('/signup', (req,res) => {
     });
 });
 
+userRouter.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    const user = User.findOne({ email }, (err, user) => {
+      if (err)
+        res
+          .status(500)
+          .json({ message: { msgBody: 'Error has occured', msgError: true } });
+    });
+    if (password !== user.password) {
+      return res.status(401).send('Incorrect Password');
+    }
+    // else
+    //     res.status(201).json({message : {msgBody : "Account successfully created", msgError : false}});
+    // res.send(user);
+  });
+
+
 userRouter.post('/', passport.authenticate('local',{session : false}), (req,res) => {
     if (req.isAuthenticated()){
         const {_id,email} = req.user;
@@ -47,41 +64,6 @@ userRouter.get('/logout', passport.authenticate('jwt',{session : false}), (req,r
     console.log('from logout')
     res.json({user:{email : "", success : true}});  
 });
-
-// userRouter.post('/mainapp', passport.authenticate('jwt',{session : false}), (req,res) => {
-//     const mainapp = new MainApp (req.body);
-//     mainapp.save(err => {
-//         if (err)
-//             res.status(500).json({message : {msgBody : "Error has occured", msgError : true}});
-//         else{
-//             req.user.todos.push(mainapp);
-//             req.user.save(err => {
-//                 if (err)
-//                     res.status(500).json({message : {msgBody : "Error has occured", msgError : true}});
-//                 else
-//                     res.status(200).json({message : {msgBody : "successful created todo", msgError : false}});
-//             });
-//         }
-//     })
-// });
-
-// userRouter.get('/mainapp', passport.authenticate('jwt',{session : false}), (req,res) => {
-//     User.findById({_id : req.user._id}).populate('todos').exec((err, document) => {
-//         if(err)
-//             res.status(500).json({message : {msgBody : "Error has occured", msgError : true}});
-//         else{
-//             res.status(200).json({todos : document.todos, authenticated : true});
-//         }
-//     });
-// });
-
-// userRouter.get('/admin', passport.authenticate('jwt',{session : false}), (req,res) => {
-//     if(req.user.role === admin){
-//         res.status(200).json({message : {msgBody : 'You ar an admin', msgError : false}});
-//     }
-//     else
-//         res.status(403).jason({message : {msgBody : 'You are not an admin, not allowed to access', msgError : true}});
-// });
 
 userRouter.get('/authenticated', passport.authenticate('jwt',{session : false}), (req,res) => {
     const {email} = req.user;
